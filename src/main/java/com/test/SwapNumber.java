73872ffcc1bb985579723abe46df13015f78c003
prabgan83@gmail.com <prabgan83@gmail.com>
2022-02-27 07:51:02 -0500

    rama

---
+++ src/main/java/com/test/SwapNumber.java
@@ -0,0 +1,18 @@
+package com.test;
+
+public class SwapNumber {
+
+	public static void main(String[] args) {
+		int x,y;
+		
+		x = 20;
+		y = 30;
+		
+		x=x+y;
+		y=x-y;
+		x=x-y;
+		System.out.println(x);
+
+	}
+
+}
